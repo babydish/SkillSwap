748c9e1d136ac0d194dd55a6cd43af4e28056df4
Hoang Tuan Anh <th469989@gmail.com>
2024-05-18 22:51:38 +0700

    deploy

--- src/index.js
+++ src/index.js
@@ -1,51 +1,56 @@
-const path = require('path');
+const path = require('path')
 const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-const { engine } = require('express-handlebars');
+const { engine } = require('express-handlebars'); // destructuring in js 
 const { Server } = require("socket.io");
-const http = require('http');
-const MongoStore = require('connect-mongo').default; // Import MongoStore
-const mongoose = require('mongoose'); // Import mongoose
-const connect_db = require('./config/db');
-const route = require('./routes');
-const { onConnected } = require('./services/socketsConnected');
-
+const { createServer } = require('node:http');
 const app = express();
-const server = http.createServer(app); // Create server using http module
+const server = require('http').createServer(app);
 const io = new Server(server);
+const connect_db = require('./config/db');
+const port = process.env.PORT || 5000; // Sử dụng PORT được cung cấp hoặc mặc định là 5000
 
-const port = process.env.PORT || 5000;
-
-connect_db.connect(); // Connect to MongoDB
+const { onConnected } = require('./services/socketsConnected')
 
 app.use(session({
-    secret: 'your-secret-key',
+    secret: 'hta28102004##',
     resave: false,
-    saveUninitialized: true,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
+    saveUninitialized: true
+
 }));
 
+
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// connect db
+connect_db.connect();
+
+const route = require('./routes');
+var morgan = require('morgan');
+// const db = require('./config/db');
+
+// // connect db
+// db.connect();
+
+
+app.use(express.urlencoded({ // body-parser ; middleware ; get data from client luu vao body
+    extended: true
+}));
+app.use(express.json()); // get date from js
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Http logger
-const morgan = require('morgan');
-app.use(morgan('combined'));
+//http logger
+app.use(morgan('combined'))
 
-// Template engine
+// template engine
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'))
 
-// Routes
+
 route(app);
 
-// Socket.io connection
 io.on('connection', (socket) => onConnected(io, socket));
-
 server.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`server running at http://localhost:${port}`);
 });
